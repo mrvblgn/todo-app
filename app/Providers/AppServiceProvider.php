3ba42cb1e691021d1a6d4fcd47b6f470f876a32e
Merve Bilgin <mrvblgn4@gmail.com>
2025-05-06 03:45:06 +0300

    Bind interfaces to implementations for Auth, Todo, and Category services in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,10 @@ use App\Services\Abstracts\ITodoService;
 use App\Services\Concretes\TodoService;
 use App\Repositories\Abstracts\ITodoRepository;
 use App\Repositories\Concretes\TodoRepository;
+use App\Services\Abstracts\ICategoryService;
+use App\Services\Concretes\CategoryService;
+use App\Repositories\Abstracts\ICategoryRepository;
+use App\Repositories\Concretes\CategoryRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +29,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IAuthRepository::class, AuthRepository::class);
 
         $this->app->bind(ITodoService::class, TodoService::class);
-        $this->app->bind(IAuthRepository::class, AuthRepository::class);
+        $this->app->bind(ITodoRepository::class, TodoRepository::class);
+
+        $this->app->bind(ICategoryService::class, CategoryService::class);
+        $this->app->bind(ICategoryRepository::class, CategoryRepository::class);
     }
 
     /**
